8791761cc23f62a55a3142e2213a93b45fefba97
Michael Ciabuca <ciabuca@gmail.com>
2020-07-20 00:50:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(num)
-  begin = 2
+  start = 2
   if num > 1
-    range = (begin..num - 1).to_a
+    range = (start..num - 1).to_a
     range.none? do |num_to_test| 
       num % num_to_test == 0
     end
